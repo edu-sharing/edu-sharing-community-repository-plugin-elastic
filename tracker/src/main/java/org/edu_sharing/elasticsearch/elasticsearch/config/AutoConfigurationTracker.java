c68608b2b3e748ea6d705935ab14ffbec64f351e
Torsten Simon <simon@edu-sharing.net>
2025-03-27 13:04:27 +0100

    fix:increase max field count (for large repos)

--- tracker/src/main/java/org/edu_sharing/elasticsearch/elasticsearch/config/AutoConfigurationTracker.java
+++ tracker/src/main/java/org/edu_sharing/elasticsearch/elasticsearch/config/AutoConfigurationTracker.java
@@ -109,7 +109,7 @@ public class AutoConfigurationTracker {
         return s.index(id -> id
                         .numberOfShards(Integer.toString(indexNumberOfShards))
                         .numberOfReplicas(Integer.toString(indexNumberOfReplicas)))
-                .mapping(mapping -> mapping.totalFields(tf -> tf.limit(10000L)))
+                .mapping(mapping -> mapping.totalFields(tf -> tf.limit(20000L)))
                 .analysis(this::getIndexSettingAnalysis);
     }
 
