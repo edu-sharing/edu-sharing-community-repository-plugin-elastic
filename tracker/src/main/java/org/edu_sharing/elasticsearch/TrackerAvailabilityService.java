2a735e0f8f80bc655f345dfb5007cd982ccc5ba7
Torsten Simon <simon@edu-sharing.net>
2025-06-04 09:23:27 +0200

    fix:readiness available for migration

--- tracker/src/main/java/org/edu_sharing/elasticsearch/TrackerAvailabilityService.java
+++ tracker/src/main/java/org/edu_sharing/elasticsearch/TrackerAvailabilityService.java
@@ -1,17 +1,22 @@
 package org.edu_sharing.elasticsearch;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.availability.*;
+import org.springframework.boot.availability.ApplicationAvailabilityBean;
+import org.springframework.boot.availability.AvailabilityState;
+import org.springframework.boot.availability.LivenessState;
+import org.springframework.boot.availability.ReadinessState;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class TrackerAvailabilityService extends ApplicationAvailabilityBean implements TrackerAvailabilityTickService {
     // curl localhost:8081/actuator/health/liveness
     private long lastTrackingEvent = System.currentTimeMillis();
     @Value("${management.endpoint.health.trackingTimeoutThreshold}")
     private long trackingTimeoutThreshold;
-
+    @Value("mode")
+    private String mode;
     @Override
     public void tick() {
         lastTrackingEvent = System.currentTimeMillis();
@@ -21,7 +26,11 @@ public class TrackerAvailabilityService extends ApplicationAvailabilityBean impl
     public <S extends AvailabilityState> S getState(Class<S> stateType) {
         S state = super.getState(stateType);
         if(state instanceof LivenessState && (System.currentTimeMillis() - lastTrackingEvent) > trackingTimeoutThreshold * 1000 * 60) {
+            log.warn("Liveness probe: Fail cause of last tracking event delay (last event: {}, diff: {}", lastTrackingEvent, System.currentTimeMillis() - lastTrackingEvent);
             return (S) LivenessState.BROKEN;
+        }
+        if(state instanceof ReadinessState && "migration-only".equals(mode)) {
+            return (S) ReadinessState.ACCEPTING_TRAFFIC;
         }
         return state;
     }
