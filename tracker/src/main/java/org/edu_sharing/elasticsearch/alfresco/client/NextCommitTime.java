bd163172607b5d5eeeb2485909fbf725aa2c003b
Daniel Rudolph <rudolph@edu-sharing.net>
2026-01-20 16:58:35 +0100

    feat: tracker by config: fix missing default constructor for NextCommitTime

--- tracker/src/main/java/org/edu_sharing/elasticsearch/alfresco/client/NextCommitTime.java
+++ tracker/src/main/java/org/edu_sharing/elasticsearch/alfresco/client/NextCommitTime.java
@@ -2,7 +2,9 @@ package org.edu_sharing.elasticsearch.alfresco.client;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class NextCommitTime {
