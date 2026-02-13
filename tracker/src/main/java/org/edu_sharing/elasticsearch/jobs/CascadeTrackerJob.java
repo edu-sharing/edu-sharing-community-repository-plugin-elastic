d8406c4dab88fd4a014f216834d29ac83c9a6147
Daniel Rudolph <rudolph@edu-sharing.net>
2026-02-13 09:23:19 +0100

    fix: CascadeTrackerJob MigrationCompletedAware

--- tracker/src/main/java/org/edu_sharing/elasticsearch/jobs/CascadeTrackerJob.java
+++ tracker/src/main/java/org/edu_sharing/elasticsearch/jobs/CascadeTrackerJob.java
@@ -3,22 +3,26 @@ package org.edu_sharing.elasticsearch.jobs;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.edu_sharing.elasticsearch.elasticsearch.core.migration.MigrationCompletedAware;
 import org.edu_sharing.elasticsearch.tracker.CascadeTracker;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @RequiredArgsConstructor
-public class CascadeTrackerJob {
+public class CascadeTrackerJob implements MigrationCompletedAware {
 
     private final CascadeTracker cascadeTracker;
 
     AtomicInteger counter = new AtomicInteger(0);
 
+    boolean migrationCompleted = false;
+
     @Scheduled(fixedDelayString="5000",scheduler = "cascadeScheduler")
     public void runJob() {
+        if(!migrationCompleted) return;
+
         int i = counter.incrementAndGet();
         log.info("Starting Job {}",i);
         cascadeTracker.track();
@@ -27,4 +31,8 @@ public class CascadeTrackerJob {
 
     }
 
+    @Override
+    public void MigrationCompleted() {
+        migrationCompleted = true;
+    }
 }
