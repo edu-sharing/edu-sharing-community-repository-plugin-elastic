ec29fc4fe3cd8099eb89241a31529f01a29f2988
Daniel Rudolph <rudolph@edu-sharing.net>
2026-04-14 12:51:23 +0200

    fix tracker reindex: reduce batchsize to prevent reindex error: coordinating_operation_bytes vs max_coordinating_and_primary_bytes

--- tracker/src/main/java/org/edu_sharing/elasticsearch/elasticsearch/core/migration/jobs/ReindexMigrationJob.java
+++ tracker/src/main/java/org/edu_sharing/elasticsearch/elasticsearch/core/migration/jobs/ReindexMigrationJob.java
@@ -42,7 +42,8 @@ public class ReindexMigrationJob implements MigrationJob {
             taskId = client.reindex(req -> req
                             .waitForCompletion(false)
                             .conflicts(Conflicts.Proceed)
-                            .source(src -> src.index(sourceIndex))
+                            .source(src -> src.index(sourceIndex)
+                            .size(1000))
                             .dest(dest -> dest.index(targetIndex)))
                     .task();
             context.setMigrationContent(taskId);
