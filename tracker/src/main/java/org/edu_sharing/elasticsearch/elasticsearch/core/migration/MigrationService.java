4a1e37d2473d12f7997ec2f0e624b2c0f05c96cc
Daniel Rudolph <rudolph@edu-sharing.net>
2026-06-18 13:04:44 +0200

    fix: MigrationService skips migration from 9.1 cause of missing authrities index

--- tracker/src/main/java/org/edu_sharing/elasticsearch/elasticsearch/core/migration/MigrationService.java
+++ tracker/src/main/java/org/edu_sharing/elasticsearch/elasticsearch/core/migration/MigrationService.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Slf4j
 @Component
@@ -88,7 +89,9 @@ public class MigrationService {
         String sourceAuthoritiesIndex = currentVersion == null ? "authorities" : "authorities_" + currentVersion;
         String sourceTransactionIndex = currentVersion == null ? "transactions" : "transactions_" + currentVersion;
 
-        if (adminService.indicesExists(sourceWorkspaceIndex, sourceTransactionIndex, sourceAuthoritiesIndex)) {
+        // leave out sourceAuthoritiesIndex check to allow migrations from 9.1
+        if (adminService.indicesExists(sourceWorkspaceIndex, sourceTransactionIndex)) {
+
 
             while (!adminService.indecesConfiguredExist()) {
                 log.info("waiting for indexes...");
@@ -243,14 +246,16 @@ public class MigrationService {
                     this::setMigrationContentDelegate
             );
 
-            jobs = List.of( // Jobs needs to be ordered by MigrationStep (see requires migration)
-                    new ReindexMigrationJob(MigrationStep.REINDEX_WORKSPACE_INDEX_PROGRESS_STEP, client, context.getSourceWorkspaceIndex(), context.getTargetWorkspaceIndex(),reindexBatchSize,requestsPerSecond),
-                    new ReindexMigrationJob(MigrationStep.REINDEX_AUTHORITIES_INDEX_PROGRESS_STEP, client, context.getSourceAuthoritiesIndex(), context.getTargetAuthoritiesIndex(),reindexBatchSize,requestsPerSecond),
-                    new ReindexMigrationJob(MigrationStep.REINDEX_TRANSACTIONS_INDEX_PROGRESS_STEP, client, context.getSourceTrackerStateIndex(), context.getTargetTrackerStateIndex(),reindexBatchSize,requestsPerSecond),
-                    new CallbackMigrationJob(client, context.getMigrationCallbacks()),
-                    new DocumentsMigrationJob(adminService, context.getMigrationTrackerStateIndex(), trackerRegistry, context.getMigrationTracker(), statusIndexServiceFactory, trackerExecutorFactory),
-                    new CompleteMigrationJob()
-            );
+            boolean shouldMigrateAuthorities = !sourceAuthoritiesIndex.split("_")[1].equals("9.1");
+
+            jobs = Stream.of( // Jobs needs to be ordered by MigrationStep (see requires migration)
+                            new ReindexMigrationJob(MigrationStep.REINDEX_WORKSPACE_INDEX_PROGRESS_STEP, client, context.getSourceWorkspaceIndex(), context.getTargetWorkspaceIndex(),reindexBatchSize,requestsPerSecond),
+                            shouldMigrateAuthorities ? new ReindexMigrationJob(MigrationStep.REINDEX_AUTHORITIES_INDEX_PROGRESS_STEP, client, context.getSourceAuthoritiesIndex(), context.getTargetAuthoritiesIndex(),reindexBatchSize,requestsPerSecond) : null,
+                            new ReindexMigrationJob(MigrationStep.REINDEX_TRANSACTIONS_INDEX_PROGRESS_STEP, client, context.getSourceTrackerStateIndex(), context.getTargetTrackerStateIndex(),reindexBatchSize,requestsPerSecond),
+                            new CallbackMigrationJob(client, context.getMigrationCallbacks()),
+                            new DocumentsMigrationJob(adminService, context.getMigrationTrackerStateIndex(), trackerRegistry, context.getMigrationTracker(), statusIndexServiceFactory, trackerExecutorFactory),
+                            new CompleteMigrationJob())
+                    .filter(Objects::nonNull).toList();
 
             validateMigrationJobs();
         }
